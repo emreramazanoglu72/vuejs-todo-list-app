8117ad0d7fa81e5add6413724c884ed30e3fc66d
emreramazanoglu72 <emreramazanoglu72@gmail.com>
2021-08-15 22:18:03 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-4 pt-4">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Yapılacaklar Listesi</h4>
+        <input
+          type="text"
+          class="form-control"
+          v-on:keyup.enter="addTodo"
+          v-model="todo_name"
+        />
+        <button class="btn btn-dark w-100 mt-4" @click="addTodo">
+          Ekle
+        </button>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li
+          v-for="item in list"
+          :key="{ item }"
+          class="list-group-item d-flex justify-content-between text-center font-weight-bold "
+          v-bind:class="[
+            item.status == 'deleted'
+              ? 'text-danger'
+              : item.status == 'success'
+              ? 'text-success'
+              : '',
+          ]"
+        >
+          {{ item.name }}
+          <span>
+            <button
+              class="btn btn-success font-weight-bold mr-2"
+              @click="successTodo(item)"
+            >
+              ✓
+            </button>
+            <button
+              class="btn btn-danger font-weight-bold"
+              @click="deleteTodo(item)"
+            >
+              X
+            </button>
+          </span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      list: [],
+      todo_name: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.todo_name.length > 1) {
+        var count = this.list.length > 0 ? this.list[this.list.length -1].id  : 1;
+        this.list.push({
+          id: count + 1,
+          name: this.todo_name,
+          status: false,
+        });
+        this.todo_name = "";
+      }
+    },
+    deleteTodo(item) {
+      console.log(this.list);
+      this.list.filter(
+        (filter) => filter.id == item.id && (filter.status = "deleted")
+      );
+    },
+    successTodo(item) {
+      this.list.filter(
+        (filter) => filter.id == item.id && (filter.status = "success")
+      );
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
